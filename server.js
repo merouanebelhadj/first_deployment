47a8591e1cb5a441aece2be09b6db76d291c472a
belhadj <belhadjmerouane703@gmail.com>
2026-04-13 15:36:42 +0100

    the first page is index not login

--- server.js
+++ server.js
@@ -1,132 +1,92 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
+
 const mongoose = require('mongoose');
+const methodOverride = require('method-override');
 
+const Customer = require("./models/customerSchema");
 
-
-app.set('view engine', 'ejs')
-app.use(express.static('public'))
+// ===== MIDDLEWARE =====
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
-var methodOverride = require('method-override')
-app.use(methodOverride('_method'))
+app.use(methodOverride('_method'));
 
-
-
-//CONNECT TO MONGODB
-
-mongoose.connect("mongodb+srv://merouane:mNb7IB5J7byYuVfH@cluster0.qsmyaqd.mongodb.net/all-data?appName=Cluster0").then(() => {
-  app.listen(port, () => {
-    console.log(`http://localhost:${port}`);
-  });
- })
- .catch((err) => {
-   console.log(err);
- });
-
-  const Customer=require("./models/customerSchema")
-
- //GET REQUEST
- app.get('/index', (req, res) => {
-  res.render('index');
-});
-
- app.get("/", (req, res) => {
-  Customer.find()
-    .then((result) => {
-      console.log(result)
-      res.render("",{arr: result});
-      })
-    .catch((err)  => {
-      console.log(err);
-      });
-  });
- 
-  app.get('/user/add.html', (req, res) => {
-  res.render('user/add');
-});
-
-app.get('/edit/:id', (req, res) => {
-  
-Customer.findById(req.params.id)
-      .then((result) => {
-      res.render("user/edit",{obj:result})
-    
-      })
-    .catch((err)  => {
-      console.log(err);
-      })
-      });
-  
-
-app.get("/user/:id", (req, res) => {
-
-    
-    Customer.findById(req.params.id)
-      .then((result) => {
-      console.log(result)
-      res.render("user/view",{obj:result})
-    
-      })
-    .catch((err)  => {
-      console.log(err);
-      });
-  });
-
-
-//POST REQUEST
-app.post('/user/add.html', (req, res) => {
- console.log(req.body)
-
- Customer.create(req.body)
- .then( result => {
-      res.redirect("/");
-    })
- .catch( err => {
-      console.log(err);
+// ===== CONNECT MONGODB =====
+mongoose.connect("mongodb+srv://merouane:mNb7IB5J7byYuVfH@cluster0.qsmyaqd.mongodb.net/all-data?appName=Cluster0")
+.then(() => {
+    app.listen(port, () => {
+        console.log(`🚀 http://localhost:${port}`);
     });
-});
-//SEARCH by id
+})
+.catch(err => console.log(err));
 
+// ================= ROUTES =================
+
+// HOME PAGE
+app.get("/", (req, res) => {
+    Customer.find()
+    .then(result => {
+        res.render("index", { arr: result });
+    })
+    .catch(err => console.log(err));
+});
+
+// INDEX PAGE
+app.get('/index', (req, res) => {
+    res.render('index');
+});
+
+// ADD PAGE
+app.get('/user/add.html', (req, res) => {
+    res.render('user/add');
+});
+
+// VIEW USER
+app.get("/user/:id", (req, res) => {
+    Customer.findById(req.params.id)
+    .then(result => {
+        res.render("user/view", { obj: result });
+    })
+    .catch(err => console.log(err));
+});
+
+// EDIT PAGE
+app.get('/edit/:id', (req, res) => {
+    Customer.findById(req.params.id)
+    .then(result => {
+        res.render("user/edit", { obj: result });
+    })
+    .catch(err => console.log(err));
+});
+
+// CREATE
+app.post('/user/add.html', (req, res) => {
+    Customer.create(req.body)
+    .then(() => res.redirect("/"))
+    .catch(err => console.log(err));
+});
+
+// SEARCH
 app.post('/search', (req, res) => {
- console.log(req.body.searchText);
-
- Customer.find({firstName:req.body.searchText})
-  .then((result) => {
-    console.log(result)
-    res.render("user/search", {arr: result});
-  }) 
-  .catch( err => {
-      console.log(err);
-    }); 
- 
+    Customer.find({ firstName: req.body.searchText })
+    .then(result => {
+        res.render("user/search", { arr: result });
+    })
+    .catch(err => console.log(err));
 });
 
-
-
-//PUT REQUEST
+// UPDATE
 app.put('/edit/:id', (req, res) => {
-  console.log(req.body)
-  Customer.updateOne({_id: req.params.id }, req.body)
-  .then((params) => {
-    res.redirect("/")
-
-
-}).catch( err => {
-    console.log(err)
-});
+    Customer.updateOne({ _id: req.params.id }, req.body)
+    .then(() => res.redirect("/"))
+    .catch(err => console.log(err));
 });
 
-//DELETE REQUEST
-
+// DELETE
 app.delete('/delete/:id', (req, res) => {
-  Customer.deleteOne({_id: req.params.id })
-  .then((params) => {
-    res.redirect("/")
-}).catch( err => {
-    console.log(err)
+    Customer.deleteOne({ _id: req.params.id })
+    .then(() => res.redirect("/"))
+    .catch(err => console.log(err));
 });
-
-  
-});
-
